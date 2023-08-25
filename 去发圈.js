954a383a0916aa844e0a853f74c3eaf6783cb41c
GlodCat <38643402+wzhjm@users.noreply.github.com>
2023-08-25 17:53:00 +0800

    Update 去发圈.js

--- 去发圈.js
+++ 去发圈.js
@@ -1,7 +1,7 @@
 /*
 
 [rewrite_local]
-^https?:\/\/api\.waixiaodian\.com\/zwyd\/common\/config url script-response-body https://github.com/wzhjm/qx_tools/main/去发圈.js
+^https?:\/\/api\.waixiaodian\.com\/zwyd\/common\/config url script-response-body https://raw.githubusercontent.com/wzhjm/qx_tools/main/去发圈.js
 
 [mitm]
 hostname = api.waixiaodian.com
